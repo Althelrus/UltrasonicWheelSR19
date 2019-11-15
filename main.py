4e85609dc19570c0c350d1edd32f24cc4cd2a4ce
Althelrus <Althelrus@gmail.com>
2019-11-14 21:04:45 -0500

    Update main.py
    Adding tasks

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ ultrasonic = DistanceSensor(echo=17, trigger=4, threshold_distance=0.5)
 
 from gpiozero import DistanceSensor
 ultrasonic = DistanceSensor(echo=17, trigger=4)
-while True:
-    print(ultrasonic.distance)
-
 
 while True:
     ultrasonic.wait_for_in_range()
     print("In range")
     ultrasonic.wait_for_out_of_range()
     print("Out of range")
+    
+    
+    
+    
+# 1 sensor def
+# 2 pump def
+# Web ui class
+# load unload config class
+# set variables
