b2b82d69ae271aca698047b9a06d19a99c679413
Althelrus <Althelrus@gmail.com>
2019-12-03 22:30:52 -0500

    adding features for live graph

--- flask-live-chart.py
+++ flask-live-chart.py
@@ -1,33 +1,14 @@
-# https://towardsdatascience.com/python-webserver-with-flask-and-raspberry-pi-398423cc6f5d
-
 import json
 from time import time
 from random import random
 from flask import Flask, render_template, make_response
 
-
-app = Flask(__name__, template_folder='html')
+app = Flask(__name__)
 
 
 @app.route('/')
-def about():
-    return render_template("home.html")
-
-
-@app.route('/about/')
-def about():
-    return render_template("about.html")
-
-
-@app.route('/setting/')
-def setting():
-    return render_template("setting.html")
-
-
-@app.route('/graph/')
 def hello_world():
     return render_template('index.html', data='test')
-
 
 @app.route('/live-data')
 def live_data():
@@ -37,6 +18,5 @@ def live_data():
     response.content_type = 'application/json'
     return response
 
-
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1', port=5000)
