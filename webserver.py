c625241bec88825a1f27dea818d66c25909c8cc5
Althelrus <Althelrus@gmail.com>
2019-12-03 20:06:38 -0500

    Updates to HTML

--- webserver.py
+++ webserver.py
@@ -25,24 +25,3 @@ def setting():
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
-
-# form = '''
-#                < form action = "post.html" method = "post" >
-#                    <input type="text" name="name"></input>
-#                    <input type="radio" name="topping" value="supreme">Supreme</input>
-#                    <input type="radio" name="topping" value="vegetarian">Vegetarian</input>
-#                    <input type="radio" name="topping" value="hawaiian">Hawaiian</input>
-#                    <select name="sauce">
-#                      <option value="tomato">Tomato</option>
-#                      <option value="barbeque">Barbeque</option>
-#                     <option value="mayonnaise">Mayonnaise</option>
-#                      <option value="secret">Secret Sauce</option>
-#                 <option selected="selected" value="secret">Secret Sauce</option>
-#                 <input type="checkbox" name="extra_cheese" value="1">Extra Cheese</input>
-#                <input type="checkbox" name="gluten_free" value="1">Gluten Free Base</input>
-#               <input type="checkbox" name="extras" value="extra_cheese">Extra Cheese</input>
-#              <input type="checkbox" name="extras" value="gluten_free">Gluten Free Base</input>
-#             <input type="checkbox" name="extras" value="extra_cheese">Extra Cheese</input>
-#            <input type="checkbox" name="extras" value="gluten_free">Gluten Free Base</input>
-# '''
-# return "<BR>" + "header /n" + form + "<BR>"
