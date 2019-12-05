08502e314e76b98c38df6ef4b0ce02a16fece7a2
Althelrus <Althelrus@gmail.com>
2019-12-05 02:35:55 -0500

    Can post data to website live on graph and as text. Also working on new form for getting constants in settings

--- static/js/highcharts.js
+++ static/js/highcharts.js
@@ -1,4 +1,4 @@
-var chart;
+var chart, chart1, chart2;
 
 /**
  * Request data from the server, add it to the graph and set a timeout
@@ -21,6 +21,40 @@ function requestData() {
         cache: false
     });
 }
+function requestData2() {
+    $.ajax({
+        url: '/live-data',
+        success: function(point) {
+            var series = chart1.series[0],
+                shift = series.data.length > 20; // shift if the series is
+                                                 // longer than 20
+
+            // add the point
+            chart1.series[0].addPoint(point, true, shift);
+
+            // call it again after one second
+            setTimeout(requestData, 1000);
+        },
+        cache: false
+    });
+}
+function requestData3() {
+    $.ajax({
+        url: '/live-data',
+        success: function(point) {
+            var series = chart2.series[0],
+                shift = series.data.length > 20; // shift if the series is
+                                                 // longer than 20
+
+            // add the point
+            chart2.series[0].addPoint(point, true, shift);
+
+            // call it again after one second
+            setTimeout(requestData, 1000);
+        },
+        cache: false
+    });
+}
 
 $(document).ready(function() {
     chart = new Highcharts.Chart({
@@ -29,6 +63,64 @@ $(document).ready(function() {
             defaultSeriesType: 'spline',
             events: {
                 load: requestData
+            }
+        },
+        title: {
+            text: 'Live random data'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Value',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'Random data',
+            data: []
+        }]
+    });
+    chart1 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'data-container2',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData2
+            }
+        },
+        title: {
+            text: 'Live random data'
+        },
+        xAxis: {
+            type: 'datetime',
+            tickPixelInterval: 150,
+            maxZoom: 20 * 1000
+        },
+        yAxis: {
+            minPadding: 0.2,
+            maxPadding: 0.2,
+            title: {
+                text: 'Value',
+                margin: 80
+            }
+        },
+        series: [{
+            name: 'Random data',
+            data: []
+        }]
+    });
+    chart2 = new Highcharts.Chart({
+        chart: {
+            renderTo: 'data-container3',
+            defaultSeriesType: 'spline',
+            events: {
+                load: requestData3
             }
         },
         title: {
--- webserver.py
+++ webserver.py
@@ -2,48 +2,100 @@
 # http://pwp.stevecassidy.net/bottle/forms-processing.html
 
 import json
-from time import time
+import time
 from random import random
-from flask import Flask, render_template, make_response, request
+import wtforms
+from bottle import redirect
+from flask import Flask, render_template, make_response, request, g, flash, url_for
 
 app = Flask(__name__, template_folder='html')
 
 
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
+    g.data_pumpIn = lambda: "%.2fV" % 12
+    g.data_pumpOut = lambda: "%.2fV" % 13
+    g.data_valveStatus = lambda: "True"
+
+
 @app.route('/')
 def home():
-    return render_template("home.html", data='test')
+    return render_template("home.html")
 
 
-@app.route('/about_application/')
+@app.route('/about')
 def about():
-    return render_template("about.html", data='test')
+    return render_template("about.html")
 
 
-@app.route('/setting_data', methods=['POST'])
+@app.route('/setting_data')
 def setting_data():
-    response = make_response(json.dumps(request.form['input'] + "/" + request.form['input1'] + "/" +  request.form['input2'] + "/" +  request.form['input3'] + "/" + request.form['input4'] + "/" +  request.form['input5'] + "/" +  request.form['input6'] + "/" +  request.form['input7'] + "/" + request.form['input8'] + "/" +  request.form['input9'] + "/" +  request.form['input10'] + "/" +  request.form['input11'] + "/" + request.form['input12'] + "/" +  request.form['input13'] + "/" +  request.form['input14'] + "/" +  request.form['checkbox[]']))
-    response.content_type = 'application/json'
-    print(response)
-    return render_template("setting.html", data=response)
+    # data = [request.form['input'], request.form['input1'], request.form['input2'],
+     #       request.form['input3'], request.form['input4'], request.form['input5']
+     #       , request.form['input6'], request.form['input7'], request.form['input8'],  request.form['input9']
+     #       , request.form['input10'], request.form['input11'], request.form['input12'],  request.form['input13']
+     #       , request.form['input14'], request.form['checkbox[]']]
+    # response = make_response(json.dumps(data))
+    # response.content_type = 'application/json'
+    # print(response)
+    # return response
+    return 1
 
 
-@app.route('/setting/')
+@app.route('/setting')
 def setting():
-    return render_template("setting.html", data='test')
+    form = RegistrationForm(request.form)
+    return render_template("setting.html", form=form)
 
 
-@app.route('/graph/')
+@app.route('/graph')
 def graph():
-    return render_template('index.html', data='test')
+    return render_template('index.html')
 
 
 @app.route('/live-data')
 def live_data():
     # Create a PHP array and echo it as JSON
-    data = [time() * 1000, random() * 100]
+    data = [time.time() * 1000, random() * 100]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
+    print(response)
     return response
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+class RegistrationForm(wtforms.Form):
+    delta = wtforms.IntegerField('Delta')
+    activewheels1 = wtforms.BooleanField('Active Wheels')
+    activewheels2 = wtforms.BooleanField('Active Wheels')
+    activewheels3 = wtforms.BooleanField('Active Wheels')
+    activewheels4 = wtforms.BooleanField('Active Wheels')
+    activewheels5 = wtforms.BooleanField('Active Wheels')
+    activewheels6 = wtforms.BooleanField('Active Wheels')
+    sensorlocation1 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    sensorlocation2 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    sensorlocation3 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    sensorlocation4 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    sensorlocation5 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    sensorlocation6 = wtforms.StringField('Sensor Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation1 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation2 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation3 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation4 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation5 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    valvelocation6 = wtforms.StringField('Valve Location', [wtforms.validators.Length(min=0, max=3)])
+    pumplocation1 = wtforms.StringField('Pump Location', [wtforms.validators.Length(min=0, max=3)])
+    pumplocation2 = wtforms.StringField('Pump Location', [wtforms.validators.Length(min=0, max=3)])
 
 
 if __name__ == '__main__':
