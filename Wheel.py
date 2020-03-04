fd3c20d5d2193f07b9099d6e29ad73b2054c2332
Althelrus <Althelrus@gmail.com>
2020-03-03 23:41:53 -0500

    Simplified Program in webserver further split code into classes in wheel and add test code

--- Wheel.py
+++ Wheel.py
@@ -1,43 +1,62 @@
-
-from ADS1256_definitions import *
-from pipyadc import ADS1256
-
-import pigpio
-pi = pigpio.pi()  # Connect to local Pi.
+import time
 
 PUMPIN = 12
 PUMPOUT = 5
 VALVE1 = 13
 VALVE_out = 6
-VALVE2 = 33
-VALVE3 = 23
-VALVE4 = 26
-VALVE5 = 13
-VALVE6 = 6
 
-pi.set_mode(PUMPIN, pigpio.OUTPUT)
-pi.set_mode(PUMPOUT, pigpio.OUTPUT)
-pi.set_mode(VALVE1, pigpio.OUTPUT)
-pi.set_mode(VALVE_out, pigpio.OUTPUT)
-pi.set_mode(VALVE3, pigpio.OUTPUT)
-pi.set_mode(VALVE4, pigpio.OUTPUT)
-pi.set_mode(VALVE5, pigpio.OUTPUT)
-pi.set_mode(VALVE6, pigpio.OUTPUT)
 
-class Wheel:
-    def read_sensor(self):
-        ads = ADS1256()
-        ads.cal_self()
-        POTI = POS_AIN0|NEG_AINCOM
+
+
+class Sensor:
+    def __init__(self):
+        from ADS1256_definitions import *
+        from pipyadc import ADS1256
+        POTI = POS_AIN0 | NEG_AINCOM
         EXT2, EXT3, EXT4 = POS_AIN2 | NEG_AINCOM, POS_AIN3 | NEG_AINCOM, POS_AIN4 | NEG_AINCOM
         EXT5, EXT6, EXT7 = POS_AIN5 | NEG_AINCOM, POS_AIN6 | NEG_AINCOM, POS_AIN7 | NEG_AINCOM
-        CH_SEQUENCE = (EXT2, EXT3, EXT4, EXT7)
-        raw_channels = ads.read_sequence(CH_SEQUENCE)
-        voltages = [i * ads.v_per_digit for i in raw_channels]
+        self.CH_SEQUENCE = (EXT2, EXT3, EXT4, EXT7)
+        self.ads = ADS1256()
+        self.ads.cal_self()
+        print("Sensor Init")
+
+    def read_sensor(self):
+        raw_channels = self.ads.read_sequence(self.CH_SEQUENCE)
+        voltages = [i * self.ads.v_per_digit for i in raw_channels]
         return voltages
 
+class Wheel:
+    def __init__(self):
+        import pigpio
+        self.pi = pigpio.pi()  # Connect to local Pi.
+        self.pi.set_mode(PUMPIN, pigpio.OUTPUT)
+        self.pi.write(PUMPIN, 0)
+        time.sleep(1)
+        self.pi.set_mode(PUMPOUT, pigpio.OUTPUT)
+        self.pi.write(PUMPOUT, 0)
+        time.sleep(1)
+        self.pi.set_mode(VALVE1, pigpio.OUTPUT)
+        self.pi.write(VALVE1, 0)
+        time.sleep(1)
+        self.pi.set_mode(VALVE_out, pigpio.OUTPUT)
+        self.pi.write(VALVE_out, 0)
+        time.sleep(1)
+        print("Wheel Init")
+
     def control_valve(self, location, value):
-        pi.write(location, value)
+        self.pi.write(location, value)
 
     def control_pump(self, location, dutyratio):
-        pi.set_PWM_dutycycle(location, dutyratio)
+        self.pi.set_PWM_dutycycle(location, dutyratio)
+
+# Test
+if __name__ == '__main__':
+    sensors = Sensor.__init__()
+    wheels = Wheel.__init__()
+    while True:
+        voltages = sensors.read_sensor()
+        print(voltages)
+        wheels.control_valve(VALVE1, 1)
+        wheels.control_pump(PUMPIN, 255)
+
+
--- webserver.py
+++ webserver.py
@@ -33,11 +33,7 @@ PUMPIN = 12
 PUMPOUT = 5
 VALVE1 = 13
 VALVE_out = 6
-VALVE2 = 33
-VALVE3 = 23
-VALVE4 = 26
-VALVE5 = 13
-VALVE6 = 6
+
 
 # speed = pwm duty cycle, 0 = off, 100 = max
 speed = 100
@@ -46,10 +42,6 @@ pi.set_mode(PUMPIN, pigpio.OUTPUT)
 pi.set_mode(PUMPOUT, pigpio.OUTPUT)
 pi.set_mode(VALVE1, pigpio.OUTPUT)
 pi.set_mode(VALVE_out, pigpio.OUTPUT)
-pi.set_mode(VALVE3, pigpio.OUTPUT)
-pi.set_mode(VALVE4, pigpio.OUTPUT)
-pi.set_mode(VALVE5, pigpio.OUTPUT)
-pi.set_mode(VALVE6, pigpio.OUTPUT)
 
 
 # todo fix load
@@ -305,7 +297,7 @@ class Wheel:
         POTI = POS_AIN0|NEG_AINCOM
         EXT2, EXT3, EXT4 = POS_AIN2 | NEG_AINCOM, POS_AIN3 | NEG_AINCOM, POS_AIN4 | NEG_AINCOM
         EXT5, EXT6, EXT7 = POS_AIN5 | NEG_AINCOM, POS_AIN6 | NEG_AINCOM, POS_AIN7 | NEG_AINCOM
-        CH_SEQUENCE = (EXT2, EXT3, EXT4, EXT7)
+        CH_SEQUENCE = (EXT2, EXT3, EXT4, EXT5, EXT6, EXT7)
         raw_channels = ads.read_sequence(CH_SEQUENCE)
         voltages = [i * ads.v_per_digit for i in raw_channels]
         return voltages
